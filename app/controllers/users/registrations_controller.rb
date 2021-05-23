dff573004c83a95e8660bbe6d647606e41f75e60
longnh <classicboy189@gmail.com>
2021-05-23 16:59:47 +0700

    Add sign up page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,23 @@
-class Users::RegistrationsController < ApplicationController
+class Users::RegistrationsController < Devise::SessionsController
   layout 'application_authentication'
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      super
+    else
+      flash.now[:warning] = t('.error')
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
